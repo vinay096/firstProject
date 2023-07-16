fa06b56a04d282c834207d9c9f0b5dd7acca63ef
vinay096 <vinaysinghyadav@06gmail.com>
2023-07-16 20:20:03 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
